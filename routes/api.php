fcbcf7f2b9112ced3e4d7ad6f77ef4f8bb9b1157
HaneenBakoor <haneenbakoor512@gmail.com>
2025-08-05 12:05:13 +0200

    Add routes

--- routes/api.php
+++ routes/api.php
@@ -1,14 +1,18 @@
 <?php
 
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\api\AuthController;
 use App\Http\Controllers\api\CategoryapiController;
 use App\Http\Controllers\api\RestaurantsapiController;
 use App\Http\Controllers\api\SubCategoryapiController;
-use Illuminate\Support\Facades\Route;
-use Illuminate\Http\Request;
 
 
 Route::get('/categories', [CategoryapiController::class, 'index']);
 Route::get('/subcategories', [SubCategoryapiController::class, 'index']);
-Route::get('/restaurants', [RestaurantsapiController::class,'index']);
-
-?>
+Route::get('/restaurants', [RestaurantsapiController::class, 'index']);
+Route::post('/register', [AuthController::class, 'SignUp']);
+Route::post('/login', [AuthController::class, 'Login']);
+Route::post('/logout', [AuthController::class, 'Logout']);
+Route::get('/restaurant/{restaurants_id}/sub_categories', [RestaurantsapiController::class, 'show']);
+Route::get('/restaurant/{restaurants_id}/sub_categories/{sub_category_id}/dishes', [RestaurantsapiController::class, 'getDishes']);
